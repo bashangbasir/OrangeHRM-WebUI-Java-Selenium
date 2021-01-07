7374eb486234bbacb14f442048dd87e2a33d755d
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2021-01-07 21:31:50 +0800

    Add ForgotPasswordTests.

--- src/test/java/forgotpassword/ForgotPasswordTests.java
+++ src/test/java/forgotpassword/ForgotPasswordTests.java
@@ -1,6 +1,24 @@
 package forgotpassword;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class ForgotPasswordTests extends BaseTest {
+
+    @Test
+    public void testForgotPasswordWithCorrectUsername(){
+        var forgotPasswordPage = homePage.clickForgotPasswordLink();
+        forgotPasswordPage.setUsername("Admin");
+        var successForgotPasswordPage = forgotPasswordPage.clickResetButton();
+        Assert.assertEquals(successForgotPasswordPage.getSuccessHeader(),"Instruction Sent !","Incorrect Success Header's content");
+        Assert.assertTrue(successForgotPasswordPage.getSuccessMessage().contains("Instructions for resetting your password have been sent to"),"Incorrect Success message's content ");
+    }
+
+    @Test
+    public void testCancelForgotPasswordProcess(){
+        var forgotPasswordPage = homePage.clickForgotPasswordLink();
+        forgotPasswordPage.clickCancelButton();
+        Assert.assertEquals(homePage.getLoginPanelHeading(),"LOGIN Panel","Incorrect page");
+    }
 }
