054d09beea1029fed39fd9b3c7ef8103f62b41e2
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2021-02-11 03:12:55 +0800

    Add method for checking the warning message displayed and method for getting warning message content.

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -9,6 +9,7 @@ public class ForgotPasswordPage {
     private By usernameTextBox = By.id("securityAuthentication_userName");
     private By resetButton = By.id("btnSearchValues");
     private By cancelButton = By.id("btnCancel");
+    private By warningMessage = By.cssSelector("div.fadable");
 
     public ForgotPasswordPage(WebDriver driver){
         this.driver = driver;
@@ -26,5 +27,13 @@ public class ForgotPasswordPage {
     public HomePage clickCancelButton(){
         driver.findElement(cancelButton).click();
         return new HomePage(driver);
+    }
+
+    public boolean isMessageWarningDisplayed(){
+        return driver.findElement(warningMessage).isDisplayed();
+    }
+
+    public String getWarningMessage(){
+        return driver.findElement(warningMessage).getText();
     }
 }
