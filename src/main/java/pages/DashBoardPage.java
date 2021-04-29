de5274b7ca4a6419e04be997a6eadf931ca9895c
bashangbasir <bassam.abd.basir@gmail.com>
2021-04-29 18:28:50 +0800

    Add method goToApplyLeaveViaLeaveMenu: to use to go to ApplyLeavePage.

--- src/main/java/pages/DashBoardPage.java
+++ src/main/java/pages/DashBoardPage.java
@@ -2,10 +2,13 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class DashBoardPage {
     private WebDriver driver;
     private By welcomeUser = By.id("welcome");
+    private By leaveMenu = By.id("menu_leave_viewLeaveModule");
+    private By applySubmenu = By.id("menu_leave_applyLeave");
 
     public DashBoardPage(WebDriver driver){
         this.driver = driver;
@@ -13,5 +16,15 @@ public class DashBoardPage {
 
     public String getWelcomeText(){
         return driver.findElement(welcomeUser).getText();
+    }
+
+    public ApplyLeavePage goToApplyLeaveViaLeaveMenu(){
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(leaveMenu))
+                .moveToElement(driver.findElement(applySubmenu))
+                .click();
+
+        actions.perform();
+        return new ApplyLeavePage(driver);
     }
 }
